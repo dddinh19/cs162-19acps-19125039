a9d0d9477887e5d72893bfcb455d7960a383454d
ntngan <64194813+ntngan@users.noreply.github.com>
2020-05-19 14:02:31 +0700

    change lecturer password

--- function.cpp
+++ function.cpp
@@ -384,3 +384,20 @@ void write_lecturer_data(lecturer* p, int n) {
 		fo.close();
 	}
 }
+void change_lecturer_password(lecturer*& p, int n, int i) {
+	char tempt[20];
+	do {
+		std::cout << " Enter your current password " << std::endl;
+		std::cin >> tempt;
+	} while (strcmp(tempt, p[i].pass) != 0);
+	char tempt_2[20];
+	std::cout << " Enter new password " << std::endl;
+	std::cin >> tempt;
+	do {
+		std::cout << " Enter new password again " << std::endl;
+		std::cin >> tempt_2;
+	} while (strcmp(tempt_2, tempt) != 0);
+	p[i].pass = new char[strlen(tempt) + 1];
+	strcpy_s(p[i].pass, strlen(tempt) + 1, tempt);
+	write_lecturer_data(p, n);
+}
