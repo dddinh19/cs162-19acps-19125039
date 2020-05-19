cf2cea35e0e8b3c3132826d0d3229b3ffc17cc83
dddinh19 <64194776+dddinh19@users.noreply.github.com>
2020-05-20 02:40:55 +0700

    task 6(full)

--- projectall/projectall/projectall.cpp
+++ projectall/projectall/projectall.cpp
@@ -1,1 +1,18 @@
+#include "function.h"
 
+int main()
+{
+	std::ifstream fi;
+	student* stu = nullptr;
+	int num = 0;
+	load_data_student(fi, stu, num);
+	for (int i = 0; i < num-1; i++)
+	{
+		std::cout << stu[i].id << std::endl;
+		std::cout << stu[i].name << std::endl;
+		std::cout << stu[i].date.year << "-" << stu[i].date.month << "-" << stu[i].date.day << std::endl;
+		std::cout << stu[i].classname << std::endl;
+	}
+	delete_data_student(stu, num);
+	return 0;
+}
