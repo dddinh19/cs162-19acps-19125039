459edf3b5cb5b2316d5162fc9751e05eeb4267e7
dddinh19 <64194776+dddinh19@users.noreply.github.com>
2020-05-23 23:13:23 +0700

    task 14(not full)

---
+++ task14projectall/task14projectall/Course.h
@@ -0,0 +1,31 @@
+#ifndef _COURSE_H_
+#define _COURSE_H_
+
+#include <iostream>
+#include <fstream>
+
+struct dob {
+	char* day, * month, * year;
+};
+struct student {
+	char* id, * pass, * name, * classname, * status;
+	dob date;
+};
+struct lecturer {
+	char* username;
+	char* password;
+	char* lecturerName;
+	char* degree;
+	char* sex;// 0 male 1 female
+	char* status;
+};
+struct course {
+	char* id, * name, * classname, * dayofweek, * room;
+	int starthour, startminute, endhour, endminute;
+	dob startdate, enddate;
+};
+
+void enterinfor(std::ifstream& fi);
+void load_data_course(std::ifstream& fi, course*& cou, int& num);
+
+#endif
