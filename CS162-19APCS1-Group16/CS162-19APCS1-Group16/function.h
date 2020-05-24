a038afbd4aebd64c8fe1dae1ee8e37cbbab296c1
qmkhoi <64194960+qmkhoi@users.noreply.github.com>
2020-05-24 13:16:05 +0700

    green comment

--- CS162-19APCS1-Group16/CS162-19APCS1-Group16/function.h
+++ CS162-19APCS1-Group16/CS162-19APCS1-Group16/function.h
@@ -1,1 +1,28 @@
-#pragma once
+#ifndef _FUNCTION_H_
+#define _FUNCTION_H_
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+//ALLROLE
+
+//CLASS
+
+//COURSE
+
+//SCOREBOARD
+
+//ATTENDANCE
+
+//LECTURER
+
+//STUDENT
+
+#endif
+
+
+
