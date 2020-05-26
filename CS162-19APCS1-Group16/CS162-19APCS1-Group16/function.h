7a4fd66f0971d6b1097974ffc805b7408a8f42ed
qmkhoi <64194960+qmkhoi@users.noreply.github.com>
2020-05-26 11:10:20 +0700

    struct

--- CS162-19APCS1-Group16/CS162-19APCS1-Group16/function.h
+++ CS162-19APCS1-Group16/CS162-19APCS1-Group16/function.h
@@ -7,7 +7,71 @@
 
 using namespace std;
 
+// STRUCT
+//ALLROLE
 
+//DATE
+struct dob {
+	int year, day, month;
+};
+
+struct time {
+	int hour, minute;
+};
+
+//CLASS
+
+
+//COURSE
+struct course {
+	string courseID;
+	string courseName;
+	lecturer lecturer_couse;
+	attendance attendance_course;
+	string dayofweek;
+	string room;
+	int status;
+};
+//SCOREBOARD
+struct scoreboad {
+	double midterm, lab, final, bonus;
+};
+//ATTENDANCE
+struct attendance {
+	dob startdate, enddate;
+	time start, end, checkin;
+};
+//LECTURER
+struct lecturer {
+	string username;
+	string password;
+	string name;
+	string degree;
+	int gender;// 0 male 1 female
+};
+
+//STUDENT
+struct student {
+	string id;
+	string pass;
+	string name;
+	dob date;
+	string classname;
+	int status;
+};
+
+//STAFF
+struct staff {
+	string username;
+	string pass;
+	string name;
+	int gender;
+};
+
+
+
+
+//FUNCTION 
 //ALLROLE
 
 //CLASS
