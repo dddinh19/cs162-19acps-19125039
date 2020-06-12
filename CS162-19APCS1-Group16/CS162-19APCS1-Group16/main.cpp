85da902ef8e450084dd724f4b6d06c6490b764cd
ntngan <64194813+ntngan@users.noreply.github.com>
2020-06-13 06:26:00 +0700

    Update main.cpp

---
+++ CS162-19APCS1-Group16/CS162-19APCS1-Group16/main.cpp
@@ -0,0 +1,18 @@
+#include "function.h"
+int main() {
+	student* p_student = nullptr;
+	int n_student = 0;
+	lecturer* p_lecturer = nullptr;
+	int n_lecturer = 0;
+	staff* p_staff = nullptr;
+	int n_staff = 0;
+	student_data(p_student, n_student);
+	staff_data(p_staff, n_staff);
+	lecturer_data(p_lecturer, n_lecturer);
+
+
+	delete[]p_student;
+	delete[]p_lecturer;
+	delete[]p_staff;
+	return 0;
+}
